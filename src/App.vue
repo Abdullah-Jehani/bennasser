5940e2d0704f459b4e50f0c93b0741a3e99e7b00
Abdullah Al-jehani <abdallahaljehani926@gmail.com>
2024-11-03 16:05:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <header>
+    <router-view></router-view>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen">
+    <div class="w-full flex justify-center h-1/2">
+      <div
+        class="w-1/2 flex justify-center items-center text-white text-3xl font-bold bg-red-500"
+      >
+        <h1>Home Page</h1>
+      </div>
+      <div
+        class="w-1/2 flex justify-center items-center text-white text-3xl font-bold bg-green-500"
+      >
+        <h1>Second Page</h1>
+      </div>
+    </div>
+  </div>
+</template>
