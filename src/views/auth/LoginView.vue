13a7b754436111d2859ede4e36a57ad41e166a98
Abdullah Al-jehani <abdallahaljehani926@gmail.com>
2024-11-05 15:35:27 +0200

    login page completed with responsiveness

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="grid grid-cols-12 w-full h-screen">
+    <!-- Content Section -->
+    <div class="md:col-start-1 md:col-span-8 col-span-full bg-white w-full">
+      <div class="p-6">
+        <h1 class="text-2xl font-bold">Bennasser</h1>
+      </div>
+      <div class="mt-24 place-items-center">
+        <h1 class="text-xl font-semibold">Login</h1>
+        <p class="mt-1 md:text-sm text-xs text-gray-500">
+          Make your house a home with our reliable home goods
+        </p>
+      </div>
+      <form class="mt-8 place-items-center w-full">
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            class="mt-4 p-2 border border-greyColor md:w-[400px] w-[300px] text-xs"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            class="mt-4 p-2 border border-greyColor md:w-[400px] w-[300px] text-xs"
+          />
+        </div>
+        <div>
+          <button
+            class="mt-4 bg-blackColor hover:bg-black text-white p-2 md:rounded-[2px] rounded text-xs md|:w-[400px] w-[300px]"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+      <div class="mt-4 place-items-center">
+        <p class="mt-8 text-gray-500 text-sm">
+          Don’t Have an Account Yet?
+          <span class="text-black underline cursor-pointer">Sign Up</span>
+        </p>
+      </div>
+    </div>
+
+    <div class="hidden md:col-start-9 md:col-span-4 md:block h-full">
+      <img
+        src="../../assets/images/slider-col-01 1.svg"
+        alt="Slider Image"
+        class="w-full h-screen object-cover"
+      />
+    </div>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 // tailwind.config.js
 module.exports = {
-  purge: [],
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        0.5: "0.5px", // Define 0.5px border width
+      },
+      colors: {
+        greyColor: "#EEEEEE",
+        blackColor: "#101010",
+      },
+    },
   },
   variants: {
     extend: {},
